640c501626d263ddc6a4d4787833f471c80c8a55
vietfuturevision-cmyk <vietfuturevision@gmail.com>
2026-03-15 16:32:46 +0700

    Add new page component for IQIPrivateOffice

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import IQIPrivateOfficeNew from "./IQIPrivateOffice";
+
+export default function Page() {
+  return <IQIPrivateOfficeNew />;
+}
+```
+
+**7.** Nhấn **Commit new file**.
+
+**8.** Kiểm tra commit hash đã đổi chưa — vào:
+```
+https://github.com/vietfuturevision-cmyk/iqi-private-office/commits/main
