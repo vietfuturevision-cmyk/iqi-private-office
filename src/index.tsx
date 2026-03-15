175f4a34c1b7e695956e6fb00b1f8b0c900447f6
vietfuturevision-cmyk <vietfuturevision@gmail.com>
2026-03-15 19:19:10 +0700

    Initialize React application with IQIPrivateOffice component

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import IQIPrivateOfficeNew from "./IQIPrivateOffice";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<IQIPrivateOfficeNew />);
